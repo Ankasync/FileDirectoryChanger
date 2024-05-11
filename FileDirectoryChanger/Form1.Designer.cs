b74e9879182bd321afa5527e35b947cfb7c1fe3f
Bob <Bob@DESKTOP-OEUVONR>
2024-05-11 10:59:37 +0300

    Proje dosyası ekle.

---
+++ FileDirectoryChanger/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace FileDirectoryChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            gösterToolStripMenuItem = new ToolStripMenuItem();
+            gizleToolStripMenuItem = new ToolStripMenuItem();
+            çıkışToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { gösterToolStripMenuItem, gizleToolStripMenuItem, çıkışToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(181, 92);
+            // 
+            // gösterToolStripMenuItem
+            // 
+            gösterToolStripMenuItem.Name = "gösterToolStripMenuItem";
+            gösterToolStripMenuItem.Size = new Size(180, 22);
+            gösterToolStripMenuItem.Text = "Göster";
+            gösterToolStripMenuItem.Click += gösterToolStripMenuItem_Click;
+            // 
+            // gizleToolStripMenuItem
+            // 
+            gizleToolStripMenuItem.Name = "gizleToolStripMenuItem";
+            gizleToolStripMenuItem.Size = new Size(180, 22);
+            gizleToolStripMenuItem.Text = "Gizle";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            çıkışToolStripMenuItem.Size = new Size(180, 22);
+            çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Shown += Form1_Shown;
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem gösterToolStripMenuItem;
+        private ToolStripMenuItem gizleToolStripMenuItem;
+        private ToolStripMenuItem çıkışToolStripMenuItem;
+    }
+}
---
+++ FileDirectoryChanger/Form1.cs
@@ -0,0 +1,27 @@
+namespace FileDirectoryChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool closeState = false;
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!closeState) e.Cancel = true;
+            this.Hide();
+
+        }
+
+        private void gösterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this
+        }
+    }
+}
