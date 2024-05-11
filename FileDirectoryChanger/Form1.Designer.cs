e753028bf429cead09f455505bdb6aa243b41532
Beytullah <anka7365@gmail.com>
2024-05-11 15:15:27 +0300

    use windows api test

--- FileDirectoryChanger/Form1.Designer.cs
+++ FileDirectoryChanger/Form1.Designer.cs
@@ -35,7 +35,17 @@
             gösterToolStripMenuItem = new ToolStripMenuItem();
             gizleToolStripMenuItem = new ToolStripMenuItem();
             çıkışToolStripMenuItem = new ToolStripMenuItem();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label3 = new Label();
+            SourceFolder = new TextBox();
+            label2 = new Label();
+            DestinationFolder = new TextBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
             contextMenuStrip1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // notifyIcon1
@@ -72,16 +82,119 @@
             çıkışToolStripMenuItem.Text = "Çıkış";
             çıkışToolStripMenuItem.Click += çıkışToolStripMenuItem_Click;
             // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            tableLayoutPanel1.Controls.Add(SourceFolder, 1, 2);
+            tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(DestinationFolder, 1, 1);
+            tableLayoutPanel1.Controls.Add(comboBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Size = new Size(511, 100);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Location = new Point(3, 66);
+            label3.Name = "label3";
+            label3.Size = new Size(249, 34);
+            label3.TabIndex = 5;
+            label3.Text = "Source Directory";
+            label3.TextAlign = ContentAlignment.TopRight;
+            // 
+            // SourceFolder
+            // 
+            SourceFolder.Dock = DockStyle.Fill;
+            SourceFolder.Location = new Point(258, 69);
+            SourceFolder.Name = "SourceFolder";
+            SourceFolder.Size = new Size(250, 29);
+            SourceFolder.TabIndex = 3;
+            SourceFolder.DoubleClick += textBox1_DoubleClick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Location = new Point(3, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(249, 33);
+            label2.TabIndex = 4;
+            label2.Text = "DestinationFolder";
+            label2.TextAlign = ContentAlignment.TopRight;
+            label2.Click += label2_Click;
+            // 
+            // DestinationFolder
+            // 
+            DestinationFolder.Dock = DockStyle.Fill;
+            DestinationFolder.Location = new Point(258, 36);
+            DestinationFolder.Name = "DestinationFolder";
+            DestinationFolder.Size = new Size(250, 29);
+            DestinationFolder.TabIndex = 1;
+            DestinationFolder.DoubleClick += textBox2_DoubleClick;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Dock = DockStyle.Fill;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "F12", "F11", "F10", "F9", "F8", "F8", "F7", "F6", "F5", "F4", "F3", "F2", "F1", "" });
+            comboBox1.Location = new Point(258, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(250, 29);
+            comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(249, 33);
+            label1.TabIndex = 2;
+            label1.Text = "Key";
+            label1.TextAlign = ContentAlignment.TopRight;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(409, 106);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 46);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(511, 229);
+            Controls.Add(button1);
+            Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
             FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
             Shown += Form1_Shown;
             contextMenuStrip1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -92,5 +205,14 @@
         private ToolStripMenuItem gösterToolStripMenuItem;
         private ToolStripMenuItem gizleToolStripMenuItem;
         private ToolStripMenuItem çıkışToolStripMenuItem;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox DestinationFolder;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private TextBox SourceFolder;
+        private Label label3;
     }
 }
--- FileDirectoryChanger/Form1.cs
+++ FileDirectoryChanger/Form1.cs
@@ -1,3 +1,8 @@
+using Microsoft.Win32.TaskScheduler;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Xml;
+
 namespace FileDirectoryChanger
 {
     public partial class Form1 : Form
@@ -6,10 +11,22 @@ namespace FileDirectoryChanger
         {
             InitializeComponent();
         }
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
         bool closeState = false;
+        bool showState = false;
+
         private void Form1_Shown(object sender, EventArgs e)
         {
-            this.Hide();
+            if (!showState)
+            {
+                this.Hide();
+
+            }
+            else
+            {
+                showState = false;
+            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -33,6 +50,161 @@ namespace FileDirectoryChanger
         {
             closeState = true;
             Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TaskService ts = new TaskService();
+            TaskDefinition td = ts.NewTask();
+            td.RegistrationInfo.Description = "Test Servis";
+
+            // oturum açýldýktan sonra tetiklenecek bir tetikleyici oluþturun
+            td.Principal.RunLevel = TaskRunLevel.Highest;
+            td.Triggers.AddNew(TaskTriggerType.Logon);
+            // Tetik her tetiklendiðinde Not Defteri'ni baþlatacak bir eylem 
+            td.Actions.Add(new ExecAction(Application.StartupPath + @"\Sesa.exe", Application.StartupPath + @"\Sesa.txt", null));
+            // Görevi kök klasöre kaydedin
+            if (!ts.RootFolder.AllTasks.Any(t => t.Name == "Sesa Task"))
+            {
+                MessageBox.Show("");
+                try
+                {
+                    ts.RootFolder.RegisterTaskDefinition(@"Sesa Task", td);
+                    MessageBox.Show("Görev Zamanlayýcý Kaydedildi");
+                }
+                catch (Exception exe)
+                {
+
+                    MessageBox.Show(exe.Message);
+                }
+
+            }
+            if (!File.Exists(Application.StartupPath + @"\Config.xml"))
+            {
+                MessageBox.Show("Select Key And Directory", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                showState = true;
+                this.Show();
+                if (File.Exists(Application.StartupPath + @"\Config.xml"))
+                {
+                    timer1.Start();
+
+                }
+
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DestinationFolder.Text == "" || comboBox1.SelectedItem.ToString() == "")
+            {
+                MessageBox.Show("Boþ Býrakýlamaz");
+            }
+            else
+            {
+                try
+                {
+                    Keys key;
+                    Enum.TryParse(comboBox1.Text, out key);
+
+                    XmlDocument doc = new XmlDocument();
+                    XmlElement Ayarlar = doc.CreateElement("Settings");
+                    doc.AppendChild(Ayarlar);
+                    XmlElement SourceFolder = doc.CreateElement("SourceFolder");
+                    Ayarlar.AppendChild(SourceFolder);
+                    XmlElement DestinationFolder = doc.CreateElement("DestinationFolder");
+                    Ayarlar.AppendChild(DestinationFolder);
+
+                    XmlElement Key = doc.CreateElement("Key");
+                    Ayarlar.AppendChild(Key);
+
+
+                    Key.InnerText = key.ToString();
+                    DestinationFolder.InnerText = this.DestinationFolder.Text;
+                    SourceFolder.InnerText = this.SourceFolder.Text;
+                    doc.Save(Application.StartupPath + @"\Config.xml");
+                    this.Close();
+                    timer1.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            XmlDocument oku = new XmlDocument();
+            oku.Load(Application.StartupPath + @"\Config.xml");
+            XmlNode tuþ = oku.GetElementsByTagName("Key")[0];
+            XmlNode SourceFolder = oku.GetElementsByTagName("SourceFolder")[0];
+            XmlNode DestinationFolder = oku.GetElementsByTagName("DestinationFolder")[0];
+            Keys key;
+            Enum.TryParse(tuþ.InnerText, out key);
+            if (tuþ.InnerText == null)
+            {
+                MessageBox.Show("Atanacak Tuþ Belirtilmedi Lütfen Configten Belirtiniz");
+            }
+            else
+            {
+
+                if (GetAsyncKeyState(key) == -32767)
+                {
+
+                    DirectoryInfo klasor = new DirectoryInfo(SourceFolder.InnerText);
+                    var dosyalar = klasor.GetFiles();
+
+                    // Dosyalarý son deðiþtirilme tarihine göre sýrala
+                    var siraliDosyalar = dosyalar.OrderByDescending(f => f.LastWriteTime);
+
+                    // En son deðiþtirilen dosyayý al
+                    var enSonDosya = siraliDosyalar.FirstOrDefault();
+                    string hedefDosyaYolu = Path.Combine(DestinationFolder.InnerText, enSonDosya.Name);
+                    try
+                    {
+                        File.Copy(enSonDosya.FullName, hedefDosyaYolu);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        return;
+                        
+                    }
+                    if(new FileInfo(hedefDosyaYolu).Length == enSonDosya.Length)
+                    MessageBox.Show("Directory Change SuccessFull");
+                    else
+                    {
+                        MessageBox.Show("Bir Sorun Oluþmuþ Olabilir Manuel Olarak Kontrol Etmenizi Tavsiye Ederiz");
+                    }
+                }
+
+            }
+        }
+
+        private void textBox2_DoubleClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            string directory = dialog.SelectedPath + @"\";
+            DestinationFolder.Text = directory;
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            string directory = dialog.SelectedPath + @"\";
+            SourceFolder.Text = directory;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
